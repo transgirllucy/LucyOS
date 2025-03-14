6d6a8f52ec3f60cc9748a26f0c78cbd39b74bd0f
Lucy von Overheidt <git@lucy.giving>
2025-03-14 14:01:19 +0100

    added hashes

--- derivations/cross_toolchain/glibc32.nix
+++ derivations/cross_toolchain/glibc32.nix
@@ -18,7 +18,7 @@ let
 
     src = pkgs.fetchurl {
       url = "https://ftp.gnu.org/gnu/glibc/glibc-2.41.tar.xz";
-      hash = "";
+      hash = "sha256-paJrIvVF1rfXs92CjhHkKPJPT6xDyTT7BxtqfQgo6QE=";
     };
 
     patchSrc = pkgs.fetchurl {
